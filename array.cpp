c6aeaaedcd0fe0b8d2ebd08c458fa277e79732eb
valensianatalia <127916135+valensianatalia@users.noreply.github.com>
2023-07-03 20:51:44 +0700

    Add files via upload

--- array.cpp
+++ array.cpp
@@ -1,43 +1,71 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int maks = 5;
-string arrayNilai[5];
+const int MAX_STACK_SIZE = 5;
+string data[MAX_STACK_SIZE];
 int top = 0;
 
-bool isFull(){
-	if(top == maks){
-		return true;
-	} else {
-		return false;
-	}
+bool isEmpty(){
+    return top == 0;
 }
 
-void push(string data){
-	if(isFull()){
-		cout<< "Data full"<<endl;
-	} else {
-		arrayNilai[top] = data;
-		top++;
-	}
+bool isFull(){
+    return top == MAX_STACK_SIZE;
 }
 
 void display(){
-cout<<"Daftar stack : "<<endl;
-for(int i = maks-1;i >= 0;i--){
-	if(arrayNilai[i] != ""){
-	cout<<"Data : "<< arrayNilai[i]<<endl;
-		}
-	}
-	cout<<"\n";
+    if(!isEmpty()){
+        cout << "Data Tersimpan : " << endl;
+        for(int a = 0; a < top; a++){
+            cout << a+1 << "."<< data[a] << endl;
+        }
+    }else{
+        cout << "Data Tidak Tersedia" << endl;
+    }
+    if(isFull()){
+        cout << "Stack Penuh" << endl;
+    }
+    cout << endl;
+}
+
+void push(){
+    if(!isFull()){
+        cout << "Masukkan Data : ";
+        cin >> data[top];
+        top++;
+    }else{
+        cout << "Stack Penuh, tidak bisa menambahkan data baru." << endl;
+    }
+}
+
+void pop(){
+    if(!isEmpty()){
+        top--;
+    }
 }
 
 int main(){
-	push("10");
-	display();
-	push("12");
-	display();
+    int pil;
 
-	cout<<"Data full ? : " <<isFull()<<endl;
+    while(true){
+        system("cls");
+        display();
+        cout  << "Menu Utama\n1. Push\n2. Pop\n3. Keluar\nPilih : ";
+        cin >> pil;
 
-	}
+        switch(pil){
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                cout << "Program Selesai" << endl;
+                return 0;
+            default:
+                cout << "Input tidak valid." << endl;
+        }
+    }
+    return 0;
+}
